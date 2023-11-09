d05e565c1c91cfdbdcf3ea6b80b185faa56b78dd
TristanMN <TristanMataNui@interia.pl>
2023-11-10 00:26:31 +0100

    Converting midi to mp3

---
+++ miditomp3.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+from midi2audio import FluidSynth
+
+audio_path = os.path.join(r"E:\Studia\PRACA INZYNIERSKA\Engineering-Thesis")
+audio_file = audio_path + "\\output.midi"
+# Ścieżka do pliku MIDI
+input_midi = audio_file
+
+# Ścieżka do pliku MP3 (po konwersji)
+output_mp3 = audio_path + "\\output.mp3"
+
+
+
+# Inicjalizacja obiektu FluidSynth
+fluidsynth = os.path.join(r"C:\fluidsynthx64\bin\fluidsynth.exe")
+soundfont_file = "C:\ProgramData\soundfonts\Roland SC-55.sf2"
+
+#soundfont_file = "C:\ProgramData\soundfonts\Gabe the Dog.sf2"
+fs = FluidSynth(soundfont_file)
+
+# Konwersja pliku MIDI na MP3
+fs.midi_to_audio(input_midi, output_mp3)
